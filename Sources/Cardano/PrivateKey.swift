0a6131ddfb84c25125cbdf62eb33072fbac7fbe6
Ostap Danylovych <music.danos@gmail.com>
2021-06-18 01:17:26 +0300

    reworked PrivateKey as fixed size

--- Sources/Cardano/PrivateKey.swift
+++ Sources/Cardano/PrivateKey.swift
@@ -8,94 +8,14 @@
 import Foundation
 import CCardano
 
-public enum PrivateKey {
-    case extended(Data)
-    case normal(Data)
-    
-    init(privateKey: CCardano.PrivateKey) {
-        switch privateKey.tag {
-        case Extended: self = .extended(privateKey.extended.copied())
-        case Normal: self = .normal(privateKey.normal.copied())
-        default: fatalError("Unknown PrivateKey type")
-        }
-    }
-    
-    var isExtended: Bool {
-        get {
-            if case .extended = self { return true }
-            return false
-        }
-    }
-    
-    var isNormal: Bool {
-        get {
-            if case .normal = self { return true }
-            return false
-        }
-    }
-    
-    public init(extendedBytes bytes: Data) throws {
-        var privateKey = try CCardano.PrivateKey(extendedBytes: bytes)
-        self = privateKey.owned()
-    }
-    
-    public init(normalBytes bytes: Data) throws {
-        var privateKey = try CCardano.PrivateKey(normalBytes: bytes)
-        self = privateKey.owned()
-    }
-    
-    public func toPublic() throws -> PublicKey {
-        try withCPrivateKey { try $0.toPublic() }
-    }
-    
-    public func bytes() throws -> Data {
-        try withCPrivateKey { try $0.bytes() }
-    }
-    
-    public func sign(message: Data) throws -> Ed25519Signature {
-        try withCPrivateKey { try $0.sign(message: message) }
-    }
-    
-    func clonedCPrivateKey() throws -> CCardano.PrivateKey {
-        try withCPrivateKey { try $0.clone() }
-    }
-    
-    func withCPrivateKey<T>(
-        fn: @escaping (CCardano.PrivateKey) throws -> T
-    ) rethrows -> T {
-        switch self {
-        case .extended(let data):
-            return try data.withCData { data in
-                var privateKey = CCardano.PrivateKey()
-                privateKey.tag = Extended
-                privateKey.extended = data
-                return try fn(privateKey)
-            }
-        case .normal(let data):
-            return try data.withCData { data in
-                var privateKey = CCardano.PrivateKey()
-                privateKey.tag = Normal
-                privateKey.normal = data
-                return try fn(privateKey)
-            }
-        }
-    }
-}
+public typealias PrivateKey = CCardano.PrivateKey
 
+extension PrivateKey: CType {}
 
-extension CCardano.PrivateKey: CPtr {
-    typealias Value = PrivateKey
-    
-    func copied() -> PrivateKey {
-        PrivateKey(privateKey: self)
-    }
-    
-    mutating func free() {
-        cardano_private_key_free(&self)
-    }
-}
+extension PrivateKey {
+    var isExtended: Bool { get { self.tag == Extended } }
+    var isNormal: Bool { get { self.tag == Normal } }
 
-extension CCardano.PrivateKey {
     public init(extendedBytes bytes: Data) throws {
         self = try bytes.withCData { bytes in
             RustResult<Self>.wrap { result, error in
@@ -133,11 +53,5 @@ extension CCardano.PrivateKey {
             }
         }.get()
         return signature.owned()
-    }
-    
-    public func clone() throws -> Self {
-        try RustResult<Self>.wrap { result, error in
-            cardano_private_key_clone(self, result, error)
-        }.get()
     }
 }
--- rust/src/private_key.rs
+++ rust/src/private_key.rs
@@ -5,39 +5,18 @@ use super::panic::*;
 use super::ptr::*;
 use super::public_key::PublicKey;
 use cardano_serialization_lib::{
-  crypto::PrivateKey as RPrivateKey, impl_mockchain::key::EitherEd25519SecretKey
+  crypto::PrivateKey as RPrivateKey, impl_mockchain::key::EitherEd25519SecretKey,
 };
 use std::convert::{TryFrom, TryInto};
 
+pub const EXTENDED_PRIVATE_KEY_LENGTH: usize = 64;
+pub const NORMAL_PRIVATE_KEY_LENGTH: usize = 32;
+
 #[repr(C)]
-#[derive(Copy)]
+#[derive(Copy, Clone)]
 pub enum PrivateKey {
-  Extended(CData),
-  Normal(CData)
-}
-
-impl Free for PrivateKey {
-  unsafe fn free(&mut self) {
-    match self {
-      PrivateKey::Extended(mut bytes) => bytes.free(),
-      PrivateKey::Normal(mut bytes) => bytes.free()
-    }
-  }
-}
-
-impl Clone for PrivateKey {
-  fn clone(&self) -> Self {
-    match self {
-      PrivateKey::Extended(bytes) => {
-        let bytes = unsafe { bytes.unowned().expect("Bad bytes pointer") };
-        PrivateKey::Extended(bytes.into())
-      }
-      PrivateKey::Normal(bytes) => {
-        let bytes = unsafe { bytes.unowned().expect("Bad bytes pointer") };
-        PrivateKey::Normal(bytes.into())
-      }
-    }
-  }
+  Extended([u8; EXTENDED_PRIVATE_KEY_LENGTH]),
+  Normal([u8; NORMAL_PRIVATE_KEY_LENGTH]),
 }
 
 impl TryFrom<PrivateKey> for RPrivateKey {
@@ -45,14 +24,8 @@ impl TryFrom<PrivateKey> for RPrivateKey {
 
   fn try_from(private_key: PrivateKey) -> Result<Self> {
     match private_key {
-      PrivateKey::Extended(bytes) => {
-        let bytes = unsafe { bytes.unowned()? };
-        RPrivateKey::from_extended_bytes(bytes).into_result()
-      }
-      PrivateKey::Normal(bytes) => {
-        let bytes = unsafe { bytes.unowned()? };
-        RPrivateKey::from_normal_bytes(bytes).into_result()
-      }
+      PrivateKey::Extended(bytes) => RPrivateKey::from_extended_bytes(&bytes).into_result(),
+      PrivateKey::Normal(bytes) => RPrivateKey::from_normal_bytes(&bytes).into_result(),
     }
   }
 }
@@ -62,62 +35,68 @@ struct TPrivateKey(EitherEd25519SecretKey);
 
 impl From<RPrivateKey> for PrivateKey {
   fn from(private_key: RPrivateKey) -> Self {
-    let bytes = private_key.as_bytes().into();
+    let bytes = private_key.as_bytes();
     let tpkey: TPrivateKey = unsafe { std::mem::transmute(private_key) };
     match tpkey.0 {
-      EitherEd25519SecretKey::Extended(_) => PrivateKey::Extended(bytes),
-      EitherEd25519SecretKey::Normal(_) => PrivateKey::Normal(bytes)
+      EitherEd25519SecretKey::Extended(_) => PrivateKey::Extended(bytes.try_into().unwrap()),
+      EitherEd25519SecretKey::Normal(_) => PrivateKey::Normal(bytes.try_into().unwrap()),
     }
   }
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn cardano_private_key_to_public(
-  private_key: PrivateKey, result: &mut PublicKey, error: &mut CError
+  private_key: PrivateKey, result: &mut PublicKey, error: &mut CError,
 ) -> bool {
   handle_exception_result(|| {
     private_key
       .try_into()
       .map(|private_key: RPrivateKey| private_key.to_public().into())
-  }).response(result, error)
+  })
+  .response(result, error)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn cardano_private_key_as_bytes(
-  private_key: PrivateKey, result: &mut CData, error: &mut CError
+  private_key: PrivateKey, result: &mut CData, error: &mut CError,
 ) -> bool {
   handle_exception_result(|| {
     private_key
       .try_into()
       .map(|private_key: RPrivateKey| private_key.as_bytes().into())
-  }).response(result, error)
+  })
+  .response(result, error)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn cardano_private_key_from_extended_bytes(
-  data: CData, result: &mut PrivateKey, error: &mut CError
+  data: CData, result: &mut PrivateKey, error: &mut CError,
 ) -> bool {
   handle_exception_result(|| {
-    data.unowned()
+    data
+      .unowned()
       .and_then(|bytes| RPrivateKey::from_extended_bytes(bytes).into_result())
       .map(|private_key| private_key.into())
-  }).response(result, error)
+  })
+  .response(result, error)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn cardano_private_key_from_normal_bytes(
-  data: CData, result: &mut PrivateKey, error: &mut CError
+  data: CData, result: &mut PrivateKey, error: &mut CError,
 ) -> bool {
   handle_exception_result(|| {
-    data.unowned()
+    data
+      .unowned()
       .and_then(|bytes| RPrivateKey::from_normal_bytes(bytes).into_result())
       .map(|private_key| private_key.into())
-  }).response(result, error)
+  })
+  .response(result, error)
 }
 
 #[no_mangle]
 pub unsafe extern "C" fn cardano_private_key_sign(
-  private_key: PrivateKey, message: CData, result: &mut Ed25519Signature, error: &mut CError
+  private_key: PrivateKey, message: CData, result: &mut Ed25519Signature, error: &mut CError,
 ) -> bool {
   handle_exception_result(|| {
     private_key
@@ -125,17 +104,6 @@ pub unsafe extern "C" fn cardano_private_key_sign(
       .zip(message.unowned())
       .map(|(private_key, message): (RPrivateKey, &[u8])| private_key.sign(message))
       .map(|ed25519_signature| ed25519_signature.into())
-  }).response(result, error)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn cardano_private_key_clone(
-  private_key: PrivateKey, result: &mut PrivateKey, error: &mut CError
-) -> bool {
-  handle_exception(|| private_key.clone()).response(result, error)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn cardano_private_key_free(private_key: &mut PrivateKey) {
-  private_key.free()
+  })
+  .response(result, error)
 }
