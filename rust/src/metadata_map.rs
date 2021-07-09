242ba5239844cd51bf7479fa6f3f3438e07efc32
Yehor Popovych <popovych.yegor@gmail.com>
2021-07-09 18:40:40 +0300

    fixed MetadataMap

--- rust/src/metadata_map.rs
+++ rust/src/metadata_map.rs
@@ -6,9 +6,39 @@ use crate::ptr::*;
 use cardano_serialization_lib::metadata::MetadataMap as RMetadataMap;
 use std::convert::{TryFrom, TryInto};
 
-pub type MetadataMapKeyValue =
-  CKeyValue<CPointer<TransactionMetadatum>, CPointer<TransactionMetadatum>>;
-pub type MetadataMap = CArray<MetadataMapKeyValue>;
+pub type MetadataMapKeyValue = CKeyValue<TransactionMetadatum, TransactionMetadatum>;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+pub struct MetadataMap {
+  pub ptr: *const std::os::raw::c_void,
+  pub len: usize
+}
+
+impl Free for MetadataMap {
+  unsafe fn free(&mut self) {
+    if self.ptr.is_null() {
+      return;
+    }
+    let mut vals = Vec::from_raw_parts(self.ptr as *mut MetadataMapKeyValue, self.len, self.len);
+    self.ptr = std::ptr::null();
+    for val in vals.iter_mut() {
+      val.free()
+    }
+  }
+}
+
+impl Ptr for MetadataMap {
+  type PT = [MetadataMapKeyValue];
+
+  unsafe fn unowned(&self) -> Result<&[MetadataMapKeyValue]> {
+    if self.ptr.is_null() {
+      Err(CError::NullPtr)
+    } else {
+      Ok(std::slice::from_raw_parts(self.ptr as *const MetadataMapKeyValue, self.len))
+    }
+  }
+}
 
 impl TryFrom<MetadataMap> for RMetadataMap {
   type Error = CError;
@@ -18,11 +48,19 @@ impl TryFrom<MetadataMap> for RMetadataMap {
     let mut metadata_map = RMetadataMap::new();
     for ckv in vec.to_vec() {
       let (tm_key, tm_value) = ckv.into();
-      let tm_key = unsafe { (*tm_key.unowned()?).try_into()? };
-      let tm_value = unsafe { (*tm_value.unowned()?).try_into()? };
-      metadata_map.insert(&tm_key, &tm_value);
+      metadata_map.insert(&tm_key.try_into()?, &tm_value.try_into()?);
     }
     Ok(metadata_map)
+  }
+}
+
+impl From<Vec<MetadataMapKeyValue>> for MetadataMap {
+  fn from(array: Vec<MetadataMapKeyValue>) -> Self {
+    let len = array.len();
+    let mut slice = array.into_boxed_slice();
+    let out = slice.as_mut_ptr();
+    std::mem::forget(slice);
+    Self { ptr: out as *const std::os::raw::c_void, len: len }
   }
 }
 
@@ -39,12 +77,22 @@ impl TryFrom<RMetadataMap> for MetadataMap {
             .into_result()
             .and_then(|tm_value| tm_value.try_into())
             .zip(tm_key.try_into())
-            .map(|(tm_value, tm_key)| (CPointer(&tm_key), CPointer(&tm_value)).into())
+            .map(|(tm_value, tm_key)| (tm_key, tm_value).into())
         })
         .collect::<Result<Vec<MetadataMapKeyValue>>>()
         .map(|metadata_map| metadata_map.into())
     })
   }
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn cardano_metadata_map_from_array(
+  array: *const MetadataMapKeyValue, len: usize,
+  result: &mut MetadataMap, error: &mut CError
+) -> bool {
+  handle_exception(|| {
+    MetadataMap { ptr: array as *const std::os::raw::c_void, len: len }
+  }).response(result, error)
 }
 
 #[no_mangle]
