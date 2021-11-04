7f8b2bcb4c5d1d2ef97086752fc8c72dffab81cf
Ostap Danylovych <music.danos@gmail.com>
2021-11-04 22:39:02 +0200

    reworked Keychain

--- Sources/Cardano/Keychain.swift
+++ Sources/Cardano/Keychain.swift
@@ -25,35 +25,60 @@ struct KeyPair {
     }
 }
 
+public enum KeychainError: Error {
+    case noSuchAccount(index: UInt32)
+}
+
 public class Keychain: SignatureProvider {
     private let _root: KeyPair
     private var _cache: [UInt32: KeyPair]
+    private var syncQueue: DispatchQueue
     
     public init(mnemonic: [String], password: Data) throws {
+        syncQueue = DispatchQueue(label: "Keychain.Sync.Queue", target: .global())
         let entropy = try Mnemonic(mnemonic: mnemonic).entropy
         _root = try KeyPair(sk: try Bip32PrivateKey(bip39: Data(entropy), password: password))
         _cache = [:]
     }
     
-    public func addAccount(index: UInt32) throws {
-        let path = Bip32Path.prefix
-        let keyPair = try _root
-            .derive(index: path.path[0])
-            .derive(index: path.path[1])
-            .derive(index: index)
-        _cache[index] = keyPair
-    }
-    
     public func accounts(_ cb: @escaping (Result<[Account], Error>) -> Void) {
         DispatchQueue.global().async {
-            let accounts = self._cache.map { (index, keyPair) in
-                Account(publicKey: keyPair.publicKey, index: index)
-            }
-            cb(.success(accounts))
+            cb(.success(self.accounts()))
         }
     }
     
     public func sign(tx: ExtendedTransaction, _ cb: @escaping (Result<Transaction, Error>) -> Void) {
         fatalError("Not implemented")
+    }
+}
+
+extension Keychain {
+    public func addAccount(index: UInt32) throws -> Account {
+        let path = Bip32Path.prefix
+        let keyPair = try _root
+            .derive(index: path.path[0])
+            .derive(index: path.path[1])
+            .derive(index: index)
+        syncQueue.sync {
+            _cache[index] = keyPair
+        }
+        return Account(publicKey: keyPair.publicKey, index: index)
+    }
+    
+    public func accounts() -> [Account] {
+        syncQueue.sync {
+            _cache.map { (index, keyPair) in
+                Account(publicKey: keyPair.publicKey, index: index)
+            }
+        }
+    }
+    
+    public func account(index: UInt32) throws -> Account {
+        return try syncQueue.sync {
+            guard let keyPair = _cache[index] else {
+                throw KeychainError.noSuchAccount(index: index)
+            }
+            return Account(publicKey: keyPair.publicKey, index: index)
+        }
     }
 }
