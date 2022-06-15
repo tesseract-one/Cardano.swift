8704d7cfaf354df7a52416057359263e3fabc956
Yehor Popovych <popovych.yegor@gmail.com>
2022-06-15 20:21:17 +0100

    updated binaries version

--- Package.swift
+++ Package.swift
@@ -56,8 +56,8 @@ let ccardano: Target = useLocalBinary ?
         path: "rust/binaries/CCardano.xcframework") :
     .binaryTarget(
         name: "CCardano",
-        url: "https://github.com/tesseract-one/Cardano.swift/releases/download/0.1.2/CCardano.binaries.zip",
-        checksum: "2352e340c34bea53d7a1877f9017d964f624b7a8d2a2fd41386ccdfa06846393")
+        url: "https://github.com/tesseract-one/Cardano.swift/releases/download/0.1.3/CCardano.binaries.zip",
+        checksum: "226023203151474e6c3788ef1c3acd677ac5f4b76dffab65080d280e8a2c37ee")
 package.targets.append(contentsOf: [
     ccardano,
     .target(
